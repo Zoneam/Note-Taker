8a145433d80464afdfb9d7b8162abd41f2c709ff
zoneam <zoneam@gmail.com>
2021-06-12 23:43:48 -0700

    fixed delete function

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -7,11 +7,11 @@ const path = require("path");
 
 router.get('/api/notes', (req, res) => res.sendFile(path.join(__dirname, '../db/db.json')));
 
-router.post('/api/notes',(req,res) => {
+router.post('/api/notes', (req, res) => {
   let newNote = req.body;
   let newId = uniqid();
   newNote.id = newId;
-  fs.readFile(path.join(__dirname, '../db/db.json'),(err,data) => {
+  fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
     if (err) throw err;
     let dbFile = JSON.parse(data);
     dbFile.push(newNote);
@@ -23,23 +23,19 @@ router.post('/api/notes',(req,res) => {
   res.redirect('/notes');
 })
 
-router.delete('/api/notes/:id', (req,res)=> {
-  let i = 0 ;
-  let db =  fs.readFileSync(path.join(__dirname, '../db/db.json'));
+router.delete('/api/notes/:id', (req, res) => {
+  let db = fs.readFileSync(path.join(__dirname, '../db/db.json'));
   let dbFile = JSON.parse(db);
   let noteId = req.params.id;
-
-    dbFile.forEach((dbItem) => {
-      if (dbItem.id.toString() === noteId){
-        dbFile.splice(i,1);
-        console.log('noteId',noteId)
-        return;
-      }
-    i++;
+  dbFile.forEach((dbItem, i) => {
+    if (dbItem.id.toString() === noteId) {
+      dbFile.splice(i, 1);
+      return;
+    }
   })
-  
-fs.writeFileSync(path.join(__dirname, '../db/db.json'),JSON.stringify(dbFile));
-res.sendStatus(200);
+
+  fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(dbFile));
+  res.sendStatus(200);
 })
 
 module.exports = router;
